05802058cbdbfe43d1ddc61814126707562e84c1
linweijian <13413039158@163.com>
2016-10-27 11:11:13 +0800

    Initial commit

---
+++ src/com/link/Hello.java
@@ -0,0 +1,12 @@
+package com.link;
+
+/**
+ * Created by linweijian on 2016/10/27.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello git hello github");
+    }
+
+}

2e29d46d86687eaa54d47169ac745bdd1eaf38ec
fastkey <13413039158@163.com>
2016-10-30 12:10:51 +0800

    提交修改

--- src/com/link/Hello.java
+++ src/com/link/Hello.java
@@ -7,8 +7,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("hello git hello github");
-        System.out.println("update by master");
-        System.out.println("update by master");
+        System.out.println("modify by master");
+        System.out.println("modify by master");
     }
 
 }
